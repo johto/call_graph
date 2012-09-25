johto/call_graph

commit 9330ef7ff7928508acd7bc49cdbb2eee48afeae5

Change references of fake_pgproc to pg_proc
I seem to have forgot to change this before I committed
48f49a99aa32b7de120338a2d12cf3ab80ada63d which introduced
create_graphs.pl.

The reason the script had fake_pgproc in the first place is that
it was a hack which allowed me to create graphs inside a database
other than the one that originally tracked the call graphs.  Maybe
in the future it would make sense to allow the user to specify a
table (similar to my fake_pgproc) to dig the function names from,
but for now, this should do..

utils/create_graphs.pl
@@ -95,10 +95,10 @@ SubGraphParams (TopLevelFunction, Callee, SubGraphID) AS (
 			unnest(\$1::text[]) SubGraphInput (val)
 	) SubGraphs (TopLevelFunction, EntryFunction)
 	JOIN
-		fake_pgproc tlf
+		pg_proc tlf
 			ON (tlf.proname = SubGraphs.TopLevelFunction)
 	JOIN
-		fake_pgproc ef
+		pg_proc ef
 			ON (ef.proname = SubGraphs.EntryFunction)
 	GROUP BY tlf.proname, ef.proname
 ),
@@ -230,8 +230,8 @@ FROM
 	SELECT
 		GraphID,
 		GraphID||'e'||Callee AS NodeID,
-		fake_pgproc.proname AS FunctionName,
-		fake_pgproc.oid AS FunctionOid,
+		pg_proc.proname AS FunctionName,
+		pg_proc.oid AS FunctionOid,
 		(TopLevelFunction, Callee) IN (SELECT TopLevelFunction, Callee FROM SubGraphParams) AS NodeIsSubGraphEntryFunction,
 		NodeIsGraphEntryFunction
 	FROM
@@ -261,8 +261,8 @@ FROM
 			SubGraphParams
 	) Edges
 	JOIN
-		fake_pgproc fake_pgproc
-			ON (fake_pgproc.oid = Edges.Callee)
+		pg_proc pg_proc
+			ON (pg_proc.oid = Edges.Callee)
 	GROUP BY
 		GraphID, TopLevelFunction, Callee, proname, oid, NodeIsGraphEntryFunction
 ) ss

commit 97a62ea791d16f69027d17c8d54443f1ad33ce8f

Fix bug with hidden oid field in create_graphs.pl
This bug was introduced by commit 9330ef7ff7928508acd7bc49cdbb2eee48afeae5.

utils/create_graphs.pl
@@ -264,7 +264,7 @@ FROM
 		pg_proc pg_proc
 			ON (pg_proc.oid = Edges.Callee)
 	GROUP BY
-		GraphID, TopLevelFunction, Callee, proname, oid, NodeIsGraphEntryFunction
+		GraphID, TopLevelFunction, Callee, proname, pg_proc.oid, NodeIsGraphEntryFunction
 ) ss
 
 ORDER BY