229ae519119a466752dbc09067144254e888d622
Marko Tiikkaja <marko@joh.to>
2012-10-02 13:46:17 +0200

    Support rendering per-relation graphs

--- utils/TableUsageGraphs.pm
+++ utils/TableUsageGraphs.pm
@@ -18,7 +18,34 @@ BEGIN
 	our @EXPORT = qw(generate_table_usage_graphs);
 }
 
-sub draw_graph
+sub draw_relationgraph
+{
+	my ($graphdir, $relid, $tlfgraphs, $relation) = @_;
+
+	open(my $pipe, "| dot -Tsvg -o $graphdir/r$relid.svg") or die "could not fork";
+
+	print $pipe "strict digraph $relation->{relname} {\n";
+	print $pipe "rankdir = LR;\n";
+
+	foreach my $tlf (@{$relation->{toplevelfunctions}})
+	{
+		print $pipe "\"$tlf\" -> \"$relid\":name;\n";
+		print $pipe "\"$tlf\" [label=\"$tlfgraphs->{$tlf}->{toplevelfunctionname}\" href=\"tlf$tlf.svg\"]\n";
+	}
+
+	print $pipe "\"$relid\" [label=\"<name> $relation->{relname}|" .
+				"sequential scans: $relation->{seq_scan}|avg seq tuples: $relation->{seq_tup_read}|" .
+				"index scans: $relation->{idx_scan}|avg idx tuples: $relation->{idx_tup_read}|" .
+				"inserted rows: $relation->{n_tup_ins}|updated rows: $relation->{n_tup_upd}|" .
+				"deleted rows: $relation->{n_tup_del}\", shape=record]\n";
+
+	print $pipe "}\n";
+
+	close($pipe);
+	die "dot failed for relation graph $relation->{relid}" if $? != 0;
+}
+
+sub draw_tlfgraph
 {
 	my ($graphdir, $toplevelfunction, $graph) = @_;
 
@@ -40,7 +67,7 @@ sub draw_graph
 					"sequential scans: $tref->{seq_scan}|avg seq tuples: $tref->{seq_tup_read}|" .
 					"index scans: $tref->{idx_scan}|avg idx tuples: $tref->{idx_tup_read}|" .
 					"inserted rows: $tref->{n_tup_ins}|updated rows: $tref->{n_tup_upd}|" .
-					"deleted rows: $tref->{n_tup_del}\"]\n";
+					"deleted rows: $tref->{n_tup_del}\", href=\"r$table.svg\"]\n";
 	}
 
 	print $pipe "}\n";
@@ -95,21 +122,50 @@ sub generate_table_usage_graphs
 SQL
 	;
 
+	my $per_relation_stats_query = 
+<<"SQL"
+	SELECT
+		relid, relname, TopLevelFunctions,
+		seq_scan, CASE WHEN seq_scan > 0 THEN round(seq_tup_read / seq_scan::numeric, 2) ELSE 0 END AS seq_tup_read,
+		idx_scan, CASE WHEN idx_scan > 0 THEN round(idx_tup_read / idx_scan::numeric, 2) ELSE 0 END AS idx_tup_read,
+		n_tup_ins, n_tup_upd, n_tup_del
+	FROM
+	(
+		SELECT
+			relid, pg_class.relname AS relname, array_agg(TopLevelFunction) AS TopLevelFunctions,
+			sum(seq_scan) AS seq_scan, sum(seq_tup_read) AS seq_tup_read,
+			sum(idx_scan) AS idx_scan, sum(idx_tup_read) AS idx_tup_read,
+			sum(n_tup_ins) AS n_tup_ins, sum(n_tup_upd) AS n_tup_upd,
+			sum(n_tup_del) AS n_tup_del
+		FROM
+			call_graph.TableUsage tu
+		JOIN
+			call_graph.CallGraphs cg
+				ON (tu.CallGraphID = cg.CallGraphID)
+		JOIN
+		    $system_catalogs->{pg_class} pg_class
+	   	     ON (pg_class.oid = tu.relid)
+		GROUP BY
+			relid, pg_class.relname
+	) ss
+SQL
+	;
+
 	my $sth = $dbh->prepare($table_list_query);
 	$sth->execute();
 
 	return undef if ($sth->rows <= 0);
 
-	my %graphs;
+	my %tlfgraphs;
 	while (my $row = $sth->fetchrow_hashref)
 	{
-		if (!exists $graphs{$row->{toplevelfunction}})
+		if (!exists $tlfgraphs{$row->{toplevelfunction}})
 		{
-			$graphs{$row->{toplevelfunction}} = { toplevelfunctionname => $row->{toplevelfunctionname},
-												  tables => {} };
+			$tlfgraphs{$row->{toplevelfunction}} = { toplevelfunctionname => $row->{toplevelfunctionname},
+													 tables => {} };
 		}
 
-		my $tables = $graphs{$row->{toplevelfunction}}{tables};
+		my $tables = $tlfgraphs{$row->{toplevelfunction}}{tables};
 
 		if (!exists $tables->{$row->{relid}})
 		{
@@ -134,13 +190,27 @@ SQL
 		}
 	}
 
-	foreach my $graph (keys %graphs)
+	$sth = $dbh->prepare($per_relation_stats_query);
+	$sth->execute();
+
+	my %relationgraphs;
+	while (my $row = $sth->fetchrow_hashref)
 	{
-		draw_graph($graphdir, $graph, $graphs{$graph})
-			if (scalar keys %{$graphs{$graph}->{tables}} > 0);
+		$relationgraphs{$row->{relid}} = $row;
 	}
 
-	return \%graphs;
+	foreach my $graph (keys %tlfgraphs)
+	{
+		draw_tlfgraph($graphdir, $graph, $tlfgraphs{$graph})
+			if (scalar keys %{$tlfgraphs{$graph}->{tables}} > 0);
+	}
+
+	foreach my $relation (keys %relationgraphs)
+	{
+		draw_relationgraph($graphdir, $relation, \%tlfgraphs, $relationgraphs{$relation});
+	}
+
+	return \%tlfgraphs;
 }
 
 END
