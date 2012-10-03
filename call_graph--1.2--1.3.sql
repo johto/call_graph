johto/call_graph

commit de1a4507e90c2d6e54d690f87c448ad36e370a27

In table usage tracking, also track DDL operations

call_graph--1.2--1.3.sql
@@ -13,22 +13,34 @@ ALTER TABLE TableUsage ADD COLUMN seq_scan bigint NOT NULL DEFAULT 0;
 ALTER TABLE TableUsage ADD COLUMN seq_tup_read bigint NOT NULL DEFAULT 0;
 ALTER TABLE TableUsage ADD COLUMN idx_scan bigint NOT NULL DEFAULT 0;
 ALTER TABLE TableUsage ADD COLUMN idx_tup_read bigint NOT NULL DEFAULT 0;
+ALTER TABLE TableUsage ADD COLUMN n_tup_ins bigint NOT NULL DEFAULT 0;
+ALTER TABLE TableUsage ADD COLUMN n_tup_upd bigint NOT NULL DEFAULT 0;
+ALTER TABLE TableUsage ADD COLUMN n_tup_del bigint NOT NULL DEFAULT 0;
 
 ALTER TABLE TableAccessBuffer ADD COLUMN seq_scan bigint NOT NULL DEFAULT 0;
 ALTER TABLE TableAccessBuffer ADD COLUMN seq_tup_read bigint NOT NULL DEFAULT 0;
 ALTER TABLE TableAccessBuffer ADD COLUMN idx_scan bigint NOT NULL DEFAULT 0;
 ALTER TABLE TableAccessBuffer ADD COLUMN idx_tup_read bigint NOT NULL DEFAULT 0;
+ALTER TABLE TableAccessBuffer ADD COLUMN n_tup_ins bigint NOT NULL DEFAULT 0;
+ALTER TABLE TableAccessBuffer ADD COLUMN n_tup_upd bigint NOT NULL DEFAULT 0;
+ALTER TABLE TableAccessBuffer ADD COLUMN n_tup_del bigint NOT NULL DEFAULT 0;
 
 -- .. and then remove the default
 ALTER TABLE TableUsage ALTER COLUMN seq_scan DROP DEFAULT;
 ALTER TABLE TableUsage ALTER COLUMN seq_tup_read DROP DEFAULT;
 ALTER TABLE TableUsage ALTER COLUMN idx_scan DROP DEFAULT;
 ALTER TABLE TableUsage ALTER COLUMN idx_tup_read DROP DEFAULT;
+ALTER TABLE TableUsage ALTER COLUMN n_tup_ins DROP DEFAULT;
+ALTER TABLE TableUsage ALTER COLUMN n_tup_upd DROP DEFAULT;
+ALTER TABLE TableUsage ALTER COLUMN n_tup_del DROP DEFAULT;
 
 ALTER TABLE TableAccessBuffer ALTER COLUMN seq_scan DROP DEFAULT;
 ALTER TABLE TableAccessBuffer ALTER COLUMN seq_tup_read DROP DEFAULT;
 ALTER TABLE TableAccessBuffer ALTER COLUMN idx_scan DROP DEFAULT;
 ALTER TABLE TableAccessBuffer ALTER COLUMN idx_tup_read DROP DEFAULT;
+ALTER TABLE TableAccessBuffer ALTER COLUMN n_tup_ins DROP DEFAULT;
+ALTER TABLE TableAccessBuffer ALTER COLUMN n_tup_upd DROP DEFAULT;
+ALTER TABLE TableAccessBuffer ALTER COLUMN n_tup_del DROP DEFAULT;
 
 -- replace ProcessCallGraphBuffers()
 CREATE OR REPLACE FUNCTION call_graph.ProcessCallGraphBuffers(_MaxBufferCount bigint)

call_graph--1.3.sql
@@ -67,7 +67,10 @@ relid oid NOT NULL,
 seq_scan bigint NOT NULL,
 seq_tup_read bigint NOT NULL,
 idx_scan bigint NOT NULL,
-idx_tup_read bigint NOT NULL
+idx_tup_read bigint NOT NULL,
+n_tup_ins bigint NOT NULL,
+n_tup_upd bigint NOT NULL,
+n_tup_del bigint NOT NULL
 
 PRIMARY KEY (CallGraphID, relid)
 );
@@ -195,12 +198,15 @@ LOOP
 		WHERE
 			CallGraphBufferID = ANY(_.CallGraphBufferIDs)
 		RETURNING
-			relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read
+			relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read,
+				   n_tup_ins, n_tup_upd, n_tup_del
 	),
 	GroupedBuffers AS (
 		SELECT
 			relid, sum(seq_scan) AS seq_scan, sum(seq_tup_read) AS seq_tup_read,
-				   sum(idx_scan) AS idx_scan, sum(idx_tup_read) AS idx_tup_read
+				   sum(idx_scan) AS idx_scan, sum(idx_tup_read) AS idx_tup_read,
+				   sum(n_tup_ins) AS n_tup_ins, sum(n_tup_upd) AS n_tup_upd,
+				   sum(n_tup_del) AS n_tup_del
 		FROM
 			Buffers
 		GROUP BY
@@ -213,7 +219,10 @@ LOOP
 			seq_scan = tu.seq_scan + buf.seq_scan,
 			seq_tup_read = tu.seq_tup_read + buf.seq_tup_read,
 			idx_scan = tu.idx_scan + buf.idx_scan,
-			idx_tup_read = tu.idx_tup_read + buf.idx_tup_read
+			idx_tup_read = tu.idx_tup_read + buf.idx_tup_read,
+			n_tup_ins = tu.n_tup_ins + buf.n_tup_ins,
+			n_tup_upd = tu.n_tup_upd + buf.n_tup_upd,
+			n_tup_del = tu.n_tup_del + buf.n_tup_del
 		FROM
 			GroupedBuffers buf
 		WHERE
@@ -221,9 +230,11 @@ LOOP
 			tu.relid = buf.relid
 	)
 	INSERT INTO
-		TableUsage (CallGraphID, relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read)
+		TableUsage (CallGraphID, relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read,
+										n_tup_ins, n_tup_upd, n_tup_del)
 	SELECT
-		_CallGraphID, relid, seq_scan, seq_tup_read, idx_scan, idx_tup_Read
+		_CallGraphID, relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read,
+							 n_tup_ins, n_tup_upd, n_tup_del
 	FROM
 		GroupedBuffers buf
 	WHERE

call_graph.c
@@ -166,15 +166,17 @@ static void process_edge_data()
 
 			planptr = SPI_prepare("INSERT INTO																			"
 								  "   call_graph.TableAccessBuffer (CallGraphBufferID, relid, seq_scan, seq_tup_read,	"
-								  "									idx_scan, idx_tup_read)								"
+								  "									idx_scan, idx_tup_read,								"
+								  "									n_tup_ins, n_tup_upd, n_tup_del)					"
 								  "SELECT																				"
 								  "   $1, relid, seq_scan, seq_tup_read,												"
 								  /* idx_* columns might be NULL if there are no indexes on the table */
-								  "	  COALESCE(idx_scan, 0), COALESCE(idx_tup_fetch, 0)									"
+								  "	  COALESCE(idx_scan, 0), COALESCE(idx_tup_fetch, 0),								"
+								  "   n_tup_ins, n_tup_upd, n_tup_del													"
 								  "FROM																					"
 								  "   pg_stat_xact_user_tables															"
 								  "WHERE																				"
-								  "   seq_scan > 0 OR idx_scan > 0														",
+								  "   GREATEST(seq_scan, idx_scan, n_tup_ins, n_tup_upd, n_tup_del) > 0					",
 								  1, argtypes);
 
 			if (!planptr)

utils/TableUsageGraphs.pm
@@ -38,15 +38,15 @@ sub draw_graph
 		my $tref = $tables->{$table};
 		print $pipe "\"$table\" [label=\"<name> $tref->{relname}|" .
 					"sequential scans: $tref->{seq_scan}|avg seq tuples: $tref->{seq_tup_read}|" .
-					"index scans: $tref->{idx_scan}|avg idx tuples: $tref->{idx_tup_read}\"]\n";
+					"index scans: $tref->{idx_scan}|avg idx tuples: $tref->{idx_tup_read}|" .
+					"inserted rows: $tref->{n_tup_ins}|updated rows: $tref->{n_tup_upd}|" .
+					"deleted rows: $tref->{n_tup_del}\"]\n";
 	}
 
 	print $pipe "}\n";
 
 	close($pipe);
 	die "dot failed for table graph $toplevelfunction" if $? != 0;
-
-	return scalar keys %{$tables};
 }
 
 sub generate_table_usage_graphs
@@ -58,7 +58,9 @@ sub generate_table_usage_graphs
 	WITH TopLevelFunctionTableUsage AS (
 	    SELECT
 	        TopLevelFunction, relid, sum(seq_scan) AS seq_scan, sum(seq_tup_read) AS seq_tup_read,
-	                                 sum(idx_scan) AS idx_scan, sum(idx_tup_read) AS idx_tup_read
+	                                 sum(idx_scan) AS idx_scan, sum(idx_tup_read) AS idx_tup_read,
+									 sum(n_tup_ins) AS n_tup_ins, sum(n_tup_upd) AS n_tup_upd,
+									 sum(n_tup_del) AS n_tup_del
 	    FROM
 	        call_graph.TableUsage tu
 	    JOIN
@@ -70,9 +72,10 @@ sub generate_table_usage_graphs
 	SELECT
 	    tu.TopLevelFunction AS TopLevelFunction, tlf.proname AS TopLevelFunctionName,
 		tu.relid AS relid, pg_class.relname AS relname,
+		pg_constraint.confrelid AS confrelid,
 		seq_scan, CASE WHEN seq_scan > 0 THEN round(seq_tup_read / seq_scan::numeric, 2) ELSE 0 END AS seq_tup_read,
 		idx_scan, CASE WHEN idx_scan > 0 THEN round(idx_tup_read / idx_scan::numeric, 2) ELSE 0 END AS idx_tup_read,
-		pg_constraint.confrelid AS confrelid
+		n_tup_ins, n_tup_upd, n_tup_del
 	FROM
 	    TopLevelFunctionTableUsage tu
 	JOIN
@@ -111,11 +114,15 @@ SQL
 		if (!exists $tables->{$row->{relid}})
 		{
 			$tables->{$row->{relid}} = { relname => $row->{relname},
+										 confrelids => [],
 										 seq_scan => $row->{seq_scan},
 										 seq_tup_read => $row->{seq_tup_read},
 										 idx_scan => $row->{idx_scan},
 										 idx_tup_read => $row->{idx_tup_read},
-										 confrelids => [] };
+										 n_tup_ins => $row->{n_tup_ins},
+										 n_tup_upd => $row->{n_tup_upd},
+										 n_tup_del => $row->{n_tup_del}
+										 };
 		}
 		else
 		{
@@ -123,6 +130,9 @@ SQL
 			$tables->{$row->{relid}}->{seq_tup_read} += $row->{seq_tup_read};
 			$tables->{$row->{relid}}->{idx_scan} += $row->{idx_scan};
 			$tables->{$row->{relid}}->{idx_tup_read} += $row->{idx_tup_read};
+			$tables->{$row->{relid}}->{n_tup_ins} += $row->{n_tup_ins};
+			$tables->{$row->{relid}}->{n_tup_upd} += $row->{n_tup_upd};
+			$tables->{$row->{relid}}->{n_tup_del} += $row->{n_tup_del};
 		}
 
 		if (defined $row->{confrelid})

commit 31001103a8b9c5a9565a7cd023055fe3f4d0aa53

Fix bug in 1.2->1.3 upgrade script
This was broken by commit de1a4507e90c2d6e54d690f87c448ad36e370a2

call_graph--1.2--1.3.sql
@@ -165,12 +165,15 @@ LOOP
 		WHERE
 			CallGraphBufferID = ANY(_.CallGraphBufferIDs)
 		RETURNING
-			relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read
+			relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read,
+				   n_tup_ins, n_tup_upd, n_tup_del
 	),
 	GroupedBuffers AS (
 		SELECT
 			relid, sum(seq_scan) AS seq_scan, sum(seq_tup_read) AS seq_tup_read,
-				   sum(idx_scan) AS idx_scan, sum(idx_tup_read) AS idx_tup_read
+				   sum(idx_scan) AS idx_scan, sum(idx_tup_read) AS idx_tup_read,
+				   sum(n_tup_ins) AS n_tup_ins, sum(n_tup_upd) AS n_tup_upd,
+				   sum(n_tup_del) AS n_tup_del
 		FROM
 			Buffers
 		GROUP BY
@@ -183,7 +186,10 @@ LOOP
 			seq_scan = tu.seq_scan + buf.seq_scan,
 			seq_tup_read = tu.seq_tup_read + buf.seq_tup_read,
 			idx_scan = tu.idx_scan + buf.idx_scan,
-			idx_tup_read = tu.idx_tup_read + buf.idx_tup_read
+			idx_tup_read = tu.idx_tup_read + buf.idx_tup_read,
+			n_tup_ins = tu.n_tup_ins + buf.n_tup_ins,
+			n_tup_upd = tu.n_tup_upd + buf.n_tup_upd,
+			n_tup_del = tu.n_tup_del + buf.n_tup_del
 		FROM
 			GroupedBuffers buf
 		WHERE
@@ -191,9 +197,11 @@ LOOP
 			tu.relid = buf.relid
 	)
 	INSERT INTO
-		TableUsage (CallGraphID, relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read)
+		TableUsage (CallGraphID, relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read,
+										n_tup_ins, n_tup_upd, n_tup_del)
 	SELECT
-		_CallGraphID, relid, seq_scan, seq_tup_read, idx_scan, idx_tup_Read
+		_CallGraphID, relid, seq_scan, seq_tup_read, idx_scan, idx_tup_read,
+							 n_tup_ins, n_tup_upd, n_tup_del
 	FROM
 		GroupedBuffers buf
 	WHERE